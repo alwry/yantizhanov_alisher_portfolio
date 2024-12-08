4b6213019362e0873e9aa501fe59da51eb429dc6
alwry <144924749+alwry@users.noreply.github.com>
2024-12-08 01:32:02 -0500

    Home page animations
    Added gsap animations to home page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-const sidebar = document.querySelector(".sidebar");
-const showSidebarButton = document.querySelector(".desktop-hidden a");
-const hideSidebarButton = document.querySelector(".sidebar li:first-child a");
+(() => {
+// const sidebar = document.querySelector(".sidebar");
+// const showSidebarButton = document.querySelector(".desktop-hidden a");
+gsap.registerPlugin(ScrollTrigger)
+const player = new Plyr('video');
+const welcomeHeading = document.querySelector('.welcome-h2');
+const welcomeText = document.querySelector('.welcome-text');
+const hailingCon = document.querySelector('#hailing-con');
 
-function showSidebar() {
-    sidebar.classList.add("show");
-  }
-  
-  function hideSidebar() {
-    sidebar.classList.remove("show");
-  }
+console.log(welcomeHeading)
 
-showSidebarButton.addEventListener("click", showSidebar);
-hideSidebarButton.addEventListener("click", hideSidebar);
+gsap.fromTo(welcomeHeading, 
+  {x: -300, opacity: 0, scale: .4}, {x: 0, opacity: 1, scale: 1, duration: .6});
+
+gsap.fromTo(welcomeText, 
+  {x: 300, opacity: 0}, {x: 0, opacity: 1, duration: .6});
+
+gsap.set(hailingCon, {opacity: 0, x: -1000});
+
+gsap.to(hailingCon, {
+    opacity: 1,
+    x: 0,
+    scrollTrigger: {
+      trigger: hailingCon,
+      start: 'top bottom',
+      end: 'bottom 60%',
+      scrub: true,
+      markers: true,
+    }
+  });
+
+
+
+// function showSidebar() {
+//     sidebar.classList.toggle("show");
+//   }
+
+// showSidebarButton.addEventListener("click", showSidebar);
+
+
+})();
